908aa0c2bb7daecc331b7c99de282aebbf5c4f04
CarlottaKiri <carlottapt@gmail.com>
2023-02-21 17:11:12 +0100

    fix: question container

--- src/components/modalScore/ModalScore.jsx
+++ src/components/modalScore/ModalScore.jsx
@@ -10,7 +10,7 @@ const ModalScore = ({ score }) => {
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setShowModal(true), 1800);
+    setTimeout(() => setShowModal(true), 180000);
   }, []);
 
   const handleButtonClick = () => {
