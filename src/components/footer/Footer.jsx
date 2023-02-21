e5d639029e12250825dcaf08b5f1e070d07b1eb0
CarlottaKiri <carlottapt@gmail.com>
2023-02-21 15:45:30 +0100

    style: footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import github from "./github.png";
 
 const Footer = () => {
   return (
@@ -7,19 +8,30 @@ const Footer = () => {
       <h1>Creators:</h1>
       <ul>
         <li>
-          <a href="https://github.com/AntonellaSchillaci">Antonella</a>
+          <a>Antonella</a>
+          <img src="./antonella.png" />
+          <a href="https://github.com/AntonellaSchillaci">
+            <img src={github} />
+          </a>
         </li>
         <li>
           <a href="https://github.com/CarlottaKiri">Carlotta</a>
+          <img src="./carlotta.png" />
+          <a href="https://github.com/CarlottaKiri">
+            <img src={github} />
+          </a>
         </li>
         <li>
           <a href="https://github.com/DavinGarozzo">Davin</a>
+          <img src="./davin.png" />
         </li>
         <li>
           <a href="https://github.com/IFranc3sca">Francesca</a>
+          <img src="./francesca.png" />
         </li>
         <li>
           <a href="https://github.com/Martina-LP">Martina</a>
+          <img src="./martina.png" />
         </li>
       </ul>
     </div>
