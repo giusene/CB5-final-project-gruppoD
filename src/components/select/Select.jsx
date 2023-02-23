97756afba081d7964f6586494b12be145cdc231a
Antonella Schillaci <antonella.schillaci145@gmail.com>
2023-02-23 11:56:48 +0100

    creazione pagina LeaderBoard(firebase)

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,28 +1,33 @@
 import { useState } from "react";
 import styles from "./styles.module.scss";
 
-const options = [
+ export const options = [
   {
+    avatar: 1,
     value: "antonella",
     title: "Captain Anto",
     image: "./antonella.png",
   },
   {
+    avatar: 2,
     value: "carlotta",
     title: "Iron Carly",
     image: "./carlotta.png",
   },
   {
+    avatar: 3,
     value: "francesca",
     title: "Fantastic Fra",
     image: "./francesca.png",
   },
   {
+    avatar: 4,
     value: "martina",
     title: "Wonder Marty",
     image: "./martina.png",
   },
   {
+    avatar: 5,
     value: "davin",
     title: "Hulk Davy",
     image: "./davin.png",
